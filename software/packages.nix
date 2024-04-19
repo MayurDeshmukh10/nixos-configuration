88f46c1c7d1b872d77722359ba2de6e6569a22e2
Mayur Deshmukh <mayur.s.deshmukh10@gmail.com>
2024-04-20 01:56:52 +0200

    Add packages

--- software/packages.nix
+++ software/packages.nix
@@ -17,18 +17,24 @@
         home-manager
         zsh-autosuggestions
 	todoist-electron
-	discord
+	#discord
 	shotwell
 	tev
 	vlc
 	git
 	neofetch
 	powertop
+	okular
 	brightnessctl # for brightness control
 	kanshi # for display configuration
+	gnome.nautilus # file manager
+	helvum # Advanced sound control
+	pavucontrol # sound control GUI
+	playerctl
+	flameshot
 	zoom-us
 	font-awesome
-	webcord
+	webcord # wayland working discord clone
 
 	];
 }
