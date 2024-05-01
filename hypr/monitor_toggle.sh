101cfa859f7fb7eef805561738f987d07cd81665
Mayur Deshmukh <mayur.s.deshmukh10@gmail.com>
2024-05-01 02:33:26 +0200

    Update hyprland config

--- hypr/monitor_toggle.sh
+++ hypr/monitor_toggle.sh
@@ -27,26 +27,26 @@ NUM_MONITORS_ACTIVE=$(hyprctl monitors | grep Monitor | wc --lines)
 
 # Turn off the laptop monitor if its on + more than one monitor is active
 # This is currently the case on startup if you use hyprland's default monitor settings
-#if [ $NUM_MONITORS_ACTIVE -gt 1 ] && hyprctl monitors | grep --quiet $INTERNAL_MONITOR; then
-#    hyprctl keyword monitor "$INTERNAL_MONITOR, disable"
-#    exit
-#fi
+if [ $NUM_MONITORS_ACTIVE -gt 1 ] && hyprctl monitors | grep --quiet $INTERNAL_MONITOR; then
+    hyprctl keyword monitor "$INTERNAL_MONITOR, disable"
+    exit
+fi
 
-#if [ $NUM_MONITORS -gt 1 ]; then  # Don't do anything if only a single monitor is detected
+if [ $NUM_MONITORS -gt 1 ]; then  # Don't do anything if only a single monitor is detected
 
-#  if hyprctl monitors | grep --quiet $EXTERNAL_MONITOR; then
-#    hyprctl keyword monitor "$EXTERNAL_MONITOR, disable"
-#    hyprctl keyword monitor "$INTERNAL_MONITOR, preferred, 0, auto"
-#  else
-#    hyprctl keyword monitor "$INTERNAL_MONITOR, disable"
-#    hyprctl keyword monitor "$EXTERNAL_MONITOR, preferred, 0, auto"
-#  fi
-#fi
+  if hyprctl monitors | grep --quiet $EXTERNAL_MONITOR; then
+    hyprctl keyword monitor "$EXTERNAL_MONITOR, disable"
+    hyprctl keyword monitor "$INTERNAL_MONITOR, preferred, 0, auto"
+  else
+    hyprctl keyword monitor "$INTERNAL_MONITOR, disable"
+    hyprctl keyword monitor "$EXTERNAL_MONITOR, preferred, 0, auto"
+  fi
+fi
 
-if $(hyprctl monitors | grep --quiet $INTERNAL_MONITOR); then
-	hyprctl keyword monitor "$INTERNAL_MONITOR, disable"
+#if $(hyprctl monitors | grep --quiet $INTERNAL_MONITOR); then
+#	hyprctl keyword monitor "$INTERNAL_MONITOR, disable"
 	#hyprctl keyword monitor "$EXTERNAL_MONITOR, 1920x1080@165, 0x0, 1"
-else
-	hyprctl keyword monitor "$INTERNAL_MONITOR, 1920x1080@60,1920x0,1.25"
+#else
+#	hyprctl keyword monitor "$INTERNAL_MONITOR, 1920x1080@60,1920x0,1.25"
 	#hyprctl keyword monitor "$EXTERNAL_MONITOR, 1920x1080@165, 0x0, 1"
-if
+#if
--- hypr/start.sh
+++ hypr/start.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+# To fix slow opening of some apps
+#sleep 2
+#killall -e xdg-desktop-portal-hyprland
+#killall -e xdg-desktop-portal-wlr
+#killall xdg-desktop-portal
+#/usr/lib/xdg-desktop-portal-hyprland &
+#sleep 2
+#/usr/lib/xdg-desktop-portal &
+
 # initializing wallpaper daemon
 hyprpaper & 
 
@@ -14,3 +23,5 @@ waybar &
 #dunst
 swaync
 
+# hypridle
+#hypridle &
